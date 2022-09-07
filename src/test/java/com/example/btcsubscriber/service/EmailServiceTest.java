4b3e8c9450cd71c4fbf38fcfd1222bfa8b08ac6a
Sergii Lisnychyi <lisnychyis@gmail.com>
2022-09-07 18:48:59 +0200

    updated email generator

--- src/test/java/com/example/btcsubscriber/service/EmailServiceTest.java
+++ src/test/java/com/example/btcsubscriber/service/EmailServiceTest.java
@@ -3,6 +3,9 @@ package com.example.btcsubscriber.service;
 import com.example.btcsubscriber.exceptions.RateException;
 import com.example.btcsubscriber.repository.SubscriptionsRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 
@@ -11,37 +14,41 @@ import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class EmailServiceTest {
+
+  @Mock
+  private JavaMailSender mailSender;
+  @Mock
+  private RateProvider rateProvider;
+  @Mock
+  private SubscriptionsRepository subscriptionsRepository;
+  @Mock
+  private EmailGenerator emailGenerator;
 
   @Test
   public void should__when_() throws RateException {
     //given
     List<String> emails = List.of("dummy@email.com");
-    JavaMailSender mailSender = mock(JavaMailSender.class);
-    RateProvider rateProvider = mock(RateProvider.class);
-    SubscriptionsRepository subscriptionsRepository = mock(SubscriptionsRepository.class);
-    EmailService emailService = new EmailService(mailSender, subscriptionsRepository, rateProvider);
+    SimpleMailMessage message = new SimpleMailMessage();
+    EmailService emailService = new EmailService(mailSender, emailGenerator, subscriptionsRepository, rateProvider);
     when(rateProvider.getRate()).thenReturn(Optional.of(10L));
     when(subscriptionsRepository.getSubscriptions()).thenReturn(emails);
+    when(emailGenerator.generateEmail(10, emails)).thenReturn(message);
     //when
     emailService.sendEmails();
     //then
-    SimpleMailMessage mailMessage = emailService.generateEmail(10L, emails);
-    verify(mailSender, times(1)).send(mailMessage);
+    verify(mailSender, times(1)).send(message);
   }
 
   @Test
   public void should_throwException_when_cantGetRates() {
     //given
-    JavaMailSender mailSender = mock(JavaMailSender.class);
-    RateProvider rateProvider = mock(RateProvider.class);
-    SubscriptionsRepository subscriptionsRepository = mock(SubscriptionsRepository.class);
-    EmailService emailService = new EmailService(mailSender, subscriptionsRepository, rateProvider);
+    EmailService emailService = new EmailService(mailSender, emailGenerator, subscriptionsRepository, rateProvider);
     given(rateProvider.getRate()).willAnswer(invocationOnMock -> {
       throw new RateException("");
     });
